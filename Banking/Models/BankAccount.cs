b9e5029afec9ccd77e7276560637d53e52255f28
stefaandc <stefaan.decock@hogent.be>
2017-10-01 22:16:46 +0200

    Create class BankAccount

---
+++ Banking/Models/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models
+{
+    public class BankAccount 
+    {
+        #region Fields
+
+        private string _accountNumber;
+        //private decimal _balance;
+       
+
+        #endregion
+
+        #region Properties
+        public decimal Balance { get; private set; }
+
+        
+
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            private set { _accountNumber = value; }
+        }
+
+        
+        #endregion
+
+
+        #region Constructor
+        public BankAccount(string account)
+        {
+            AccountNumber = account;
+            Balance = Decimal.Zero;
+            
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+            
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            
+        }
+
+       
+        #endregion
+
+    }
+}
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,15 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("123-4567890-12");
+            Console.WriteLine($"Accountnumber: {account.AccountNumber}");
+            Console.WriteLine($"Balance: {account.Balance}");
+            account.Deposit(200M);
+            Console.WriteLine($"Balance after deposit of 200€: {account.Balance}");
+            account.Withdraw(100M);
+            Console.WriteLine($"Balance after withdraw of 100€: {account.Balance}");
+            
+            Console.ReadKey();
         }
     }
 }
